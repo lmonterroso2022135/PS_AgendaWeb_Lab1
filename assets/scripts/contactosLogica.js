466b84e77ba2860371f466509d51e0bd5de81739
lmonterroso2022135 <lmonterroso-2022135@kinal.edu.gt>
2024-02-04 18:07:50 -0600

    Conexion entre paginas

---
+++ assets/scripts/contactosLogica.js
@@ -0,0 +1,9 @@
+function ventanaTareas(){
+    window.location.href = "../../assets/pages/tareas.html";
+};
+function ventanaNuevo(){
+    window.location.href = "../../assets/pages/nuevoContacto.html";
+};
+function ventanaFavoritos(){
+    window.location.href = "../../assets/pages/contactosFavortios.html";
+};
